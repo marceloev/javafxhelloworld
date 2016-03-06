efd1dd6accd0fc96bd5968f93d4a57438eb1fee7
Markus KARG <markus@headcrashing.eu>
2016-03-06 18:03:10 +0100

    Fixed git problem with local links
    Apparently git uploaded a local link instead of the actual file content.

--- src/main/java/eu/headcrashing/HelloWorldApplication.java
+++ src/main/java/eu/headcrashing/HelloWorldApplication.java
@@ -1,1 +1,30 @@
-/tmp/VirtualBox Dropped Files/2016-03-06T16:36:15.667043000Z/HelloWorldApplication.java
+package eu.headcrashing;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloWorldApplication extends Application {
+
+	public static void main(final String[] arguments) {
+		Application.launch(arguments);
+	}
+
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		final Class<?> myClass = this.getClass();
+		final String myPackage = myClass.getPackage().getName();
+		final URL fxml = myClass.getResource("HelloWorldView.fxml");
+		final ResourceBundle resources = ResourceBundle.getBundle(myPackage + ".HelloWorldResources");
+		final Parent rootNode = FXMLLoader.load(fxml, resources);
+		final Scene view = new Scene(rootNode);
+		primaryStage.setScene(view);
+		primaryStage.show();
+	}
+
+}
